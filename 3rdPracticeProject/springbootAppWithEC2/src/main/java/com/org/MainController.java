ed4a710f65c0c3605a47db9f5069c4ed80ae58e3
unknown <saud.ali555@gmail.com>
2022-03-23 20:05:21 +0530

    Deploy application on cloud

---
+++ 3rdPracticeProject/springbootAppWithEC2/src/main/java/com/org/MainController.java
@@ -0,0 +1,14 @@
+package com.org;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@GetMapping("/")
+	public String hello()
+	{
+		return "Helloo from AWS";
+	}
+}
---
+++ 3rdPracticeProject/springbootAppWithEC2/src/test/java/com/org/SpringbootAppWithEc2ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.org;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest(classes = SpringbootAppWithEc2ApplicationTests.class)
+@ContextConfiguration
+class SpringbootAppWithEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
